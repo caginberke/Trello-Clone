c27ce5b24f6c77922701d4e915b0094f869c6dfa
Çağın <caginberke416@gmail.com>
2023-11-18 16:45:48 +0300

    Setup

---
+++ trello-clone/app/(marketing)/page.tsx
@@ -0,0 +1,11 @@
+const MarketingPage = () => {
+    return ( 
+
+        <div className="flex items-center justify-center flex-col">
+            Marketing Page
+        </div>
+
+     );
+}
+ 
+export default MarketingPage;
