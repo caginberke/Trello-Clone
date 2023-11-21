761752da42573a97e149537ca05b7c8da72f4bda
Çağın <caginberke416@gmail.com>
2023-11-21 17:55:24 +0300

    .

---
+++ trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,14 @@
+import { OrgControl } from "./_components/org-control";
+
+const OrganizationIdLayout = ({ children }: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div className="">
+            <OrgControl/>
+            {children}
+        </div>
+    )
+}
+
+export default OrganizationIdLayout
